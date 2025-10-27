c206f794c93025befde1c0103888cf9278a3d238
Chinedu Christopher <chichris28@gmail.com>
2025-10-27 21:47:38 +0100

    Final Update

--- day1_basics.js
+++ day1_basics.js
@@ -1,39 +1,39 @@
 //Program 1: Checks if a number is even or odd
-// const readline = require("readline");
+const readline = require("readline");
 
-// const r1 = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// r1.question("Input Number: ", (answer) =>{
-//     if((parseInt(answer))%2 == 0){
-//         console.log("The number is Even!");
-//     }else{
-//         console.log("The number is Odd!");
-//     }
-//     r1.close;
-// });
+r1.question("Input Number: ", (answer) =>{
+    if((parseInt(answer))%2 == 0){
+        console.log("The number is Even!");
+    }else{
+        console.log("The number is Odd!");
+    }
+    r1.close;
+});
 
 //second variation using prompt (for web)
-// let num = prompt("Input Number: ");
-// if((parseInt(num))%2 == 0){
-//     console.log("The Number is Even");
-// }else{
-//     console.log("The number is Odd")
-// }
-
-//Program 2: Print all numbers from 1 to 100 that are divisible by both 3 and 5.
-// for (let index = 1; index <= 100; index++) {
-//         if((index%3==0) && (index%5==0)){
-//             console.log(index);
-//         }
-// }
-
-//Program 3: String Reversal Function. returns the reverse version of the the input.
-
-function reverseString(str){
-    console.log(str.reverse())
+let num = prompt("Input Number: ");
+if((parseInt(num))%2 == 0){
+    console.log("The Number is Even");
+}else{
+    console.log("The number is Odd")
 }
 
-reverseString("hello");
+//Program 2: Print all numbers from 1 to 100 that are divisible by both 3 and 5.
+for (let index = 1; index <= 100; index++) {
+        if((index%3==0) && (index%5==0)){
+            console.log(index);
+        }
+}
+
+//Program 3: String Reversal Function. returns the reverse version of the the input.
+function reverseString(str) {
+  let txt = str.split('').reverse().join('');
+  return txt;
+}
+
+console.log(reverseString("the text goes in here...!"));
